6b96ad13b2f0a00c754be5527e240942783a90b8
frolleks <frolleks@hotmail.com>
2022-12-27 16:00:43 +0700

    chore: revert back to using react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+const Login = React.lazy(() => import('./pages/Login/Login'));
+
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
