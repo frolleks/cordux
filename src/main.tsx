f8ccc21ff4d0a961a56809614befc451616acfb3
frolleks <frolleks@hotmail.com>
2022-12-28 13:56:31 +0700

    refactor: add root css

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom';
 import router from './Routes';
+import "./index.css"
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
