9a5d63c969fe30611eb193248a15fed30bc4f633
frolleks <frolleks@hotmail.com>
2022-12-28 11:51:42 +0700

    fix: make login fully working

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,10 @@
-import React from "react";
-import Modal from 'react-modal';
+import React, { useEffect } from "react";
+import Modal from "react-modal";
+import { useNavigate } from 'react-router-dom';
 import axios, { AxiosResponse } from "axios";
-import "./Login.css"
+import HCaptcha from '@hcaptcha/react-hcaptcha';
+import "./Login.css";
+import config from "../../../config.json"
 
 interface LoginSchema {
   login: string;
@@ -18,84 +21,132 @@ interface MFALoginSchema {
 }
 
 type APILoginResponse = {
-  token: string | null,
-  settings: any,
-  ticket?: string | undefined, // MFA ticket
-  sms?: boolean | undefined,
-  mfa?: boolean | undefined,
-}
+  token: string | null;
+  settings: any;
+  ticket?: string | undefined; // MFA ticket
+  sms?: boolean | undefined;
+  mfa?: boolean | undefined;
+};
 
 function Login() {
-  const [login, setEmail] = React.useState("");
+  const [showModal, setShowModal] = React.useState(false);
+  const [ticket, setTicket] = React.useState("");
+  const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
   const [code, setCode] = React.useState("");
-  const [showModal, setShowModal] = React.useState(false);
+  const [captchaKey, setCaptchaKey] = React.useState("");
+
+  const navigate = useNavigate();
+
+  const handleCaptcha = (token: string) => {
+    setCaptchaKey(token);
+  };
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      // User is already logged in, redirect them to the /app route
+      navigate("/app");
+    }
+  }, [navigate]);
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     try {
-      const response: AxiosResponse<APILoginResponse> = await axios.post(
-        "https://discord.com/api/v9/auth/login",
-        {
-          login,
-          password,
-        }
-      );
-      if (response.data.mfa) {
-        // Display the 2FA code input field
-        setShowModal(true);
-      } else {
+      if (showModal) {
+        // Log in with the 2FA code
+        const mfaLoginPayload: MFALoginSchema = {
+          code,
+          ticket,
+        };
+        const response: AxiosResponse<APILoginResponse> = await axios.post(
+          "https://discord.com/api/v9/auth/mfa/totp",
+          mfaLoginPayload
+        );
+        // Save the token and log in
         const token = response.data.token;
-        localStorage.setItem('user_token', token);
-        // Your success code here
+        // @ts-ignore
+        localStorage.setItem("token", token);
+        navigate('/app');
+      } else {
+        // Log in with the email and password
+        const loginPayload: LoginSchema = {
+          login: email,
+          password,
+          captcha_key: captchaKey,
+        };
+        const response: AxiosResponse<APILoginResponse> = await axios.post(
+          "https://discord.com/api/v9/auth/login",
+          loginPayload
+        );
+        if (response.data.ticket) {
+          // Save the MFA ticket and show the 2FA code modal
+          const mfaTicket = response.data.ticket;
+          setTicket(mfaTicket);
+          setShowModal(true);
+        } else {
+          // Save the token and log in
+          const token = response.data.token;
+          // @ts-ignore
+          localStorage.setItem("discord_token", token);
+          navigate('/app');
+        }
       }
+      
     } catch (error) {
-      console.error(`You've encountered an error: ${error}`)
+      // Handle the error
+      console.error(error);
     }
-  };
-
-  const handleCloseModal = () => {
-    // Close the 2FA code modal
-    setShowModal(false);
   };
 
   return (
     <>
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="email">Email:</label>
-      <input
-        type="email"
-        id="email"
-        placeholder="Enter your email"
-        value={login}
-        onChange={(event) => setEmail(event.target.value)}
-      />
-      <br />
-      <label htmlFor="password">Password:</label>
-      <input
-        type="password"
-        id="password"
-        placeholder="Enter your password"
-        value={password}
-        onChange={(event) => setPassword(event.target.value)}
-      />
-      <br />
-      <button type="submit">Login</button>
-    </form>
-    <Modal isOpen={showModal} onRequestClose={handleCloseModal}>
-      <input
-        type="text"
-        id="code"
-        placeholder="Enter your two-factor authentication code/backup code"
-        value={code}
-        onChange={(event) => setCode(event.target.value)}
-      />
-    </Modal>
-    <footer>
-      <p>Image by <a href="https://unsplash.com/@randomlies">Ashim D'Silva</a> on <a href="https://unsplash.com">Unsplash</a></p>
-    </footer>
+      <div>
+        {/* Modal for 2FA code input */}
+        <Modal isOpen={showModal} onRequestClose={() => setShowModal(false)}>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="code">2FA Code:</label>
+            <input
+              type="text"
+              name="code"
+              value={code}
+              onChange={(event) => setCode(event.target.value)}
+            />
+            <button type="submit">Log In</button>
+          </form>
+        </Modal>
+        {/* Login form */}
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          {captchaKey ? (
+            <input type="hidden" name="captcha_key" value={captchaKey} />
+          ) : (
+            <HCaptcha sitekey={config.hCaptchaSiteKey} onVerify={handleCaptcha} />
+          )}
+          <button type="submit">Log In</button>
+        </form>
+      </div>
+      <footer>
+        <p>
+          Image by <a href="https://unsplash.com/@randomlies">Ashim D'Silva</a>{" "}
+          on <a href="https://unsplash.com">Unsplash</a>
+        </p>
+      </footer>
     </>
   );
-};
+}
 
 export default Login;
