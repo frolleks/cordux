425d83ee58a2bbfc5608d5c935330cd370d707b5
frolleks <frolleks@hotmail.com>
2022-12-28 11:54:47 +0700

    feat: add a loading screen (does not work)

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,11 @@
+import Lottie from "lottie-react";
+import LoadingAnimation from "./animation.json";
+
+function Loader() {
+  return (
+    // <Lottie animationData={LoadingAnimation} />
+    <h1>Loading...</h1>
+  );
+}
+
+export default Loader
